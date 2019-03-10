bf019f9d9015fa0230de480a3b022349a4db4518
diniyo804 <diniyo@naver.com>
2019-03-11 05:55:54 +0900

    무기장착, 몬스터사냥 메서드 추가

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -6,11 +6,20 @@ public interface UserService {
 
     //유저생성
     User newUser();
+
     //종족선택
     User selectUserType(User user);
+
     //경험치 계산과 레벨업
     User addExpAndLevelUp(User user, int addExp);
 
     //2차직업설정
     User newPosition(User user);
+
+    //무기장착
+    User addWeapon(User user);
+
+    //2차 전직 후 사냥
+    User HuntingMonsters(User user);
+
 }
--- src/main/java/service/UserServiceImple.java
+++ src/main/java/service/UserServiceImple.java
@@ -7,6 +7,7 @@ import userType.ElfType;
 import userType.HumanType;
 import userType.OrcType;
 import userType.Type;
+import weapons.*;
 
 import java.util.Scanner;
 
@@ -14,6 +15,7 @@ public class UserServiceImple implements UserService {
 
     private static UserService service = new UserServiceImple();
     private Scanner scanner = new Scanner(System.in);
+    int pick;
 
     public static UserService getUserServiceInstance() {
         if (service == null) {
@@ -27,17 +29,17 @@ public class UserServiceImple implements UserService {
         System.out.println(">input your name");
         String name = scanner.next();
         User user = new User(name);
-        System.out.println(">created the \"" + user.getName() + "\"");
+        System.out.println("----------------------------------------------------");
+        System.out.println("*** created the \"" + user.getName() + "\" ***");
+        System.out.println("----------------------------------------------------");
         return user;
     }
 
-
     //종족 선택
     public User selectUserType(User user) {
-        int pick;
         Type type = null;
 
-        System.out.println(">select your Character Type \n >1.Human   2.Orc   3.Elf ");
+        System.out.println(">select your Character Type \n>1.인간   2.오크   3.엘프 ");
         pick = scanner.nextInt();
 
         if (pick == Rule.PICK_HUMAN) {
@@ -49,13 +51,14 @@ public class UserServiceImple implements UserService {
         }
         user.setUserType(type);
         //종족정보확인 test
-        System.out.println(">your character type : " + user.getTypeToString());
+        System.out.println("----------------------------------------------------");
+        System.out.println(">your character type : < " + user.getTypeToString() +" >");
         return user;
     }
 
-
     //경험치 계산 및 레벨업
     public User addExpAndLevelUp(User user, int addExp) {
+
         //누적 경험치 = 기본경험치 + 추가경험치
         int allExp = user.getBaseExp() + addExp;
         //레벨업 수치
@@ -69,39 +72,93 @@ public class UserServiceImple implements UserService {
 
         if (user.isMaxLevel()) user.setMaxLevel();
 
-        System.out.println(user.getName() + "의 레벨 :" + user.getLevel());
-
         return user;
     }
 
-    //2차직업
+    //2차직업설정
     public User newPosition(User user) {
         String type = user.getTypeToString();
         Position position = null;
-        int pick;
 
         System.out.println(">select your new position");
 
         if (type == Rule.HUMAN) {
-            System.out.println(">1.전사   2.마법사");
+            System.out.println(">[1]전사   [2]마법사");
             pick = scanner.nextInt();
-            if (pick == 1) position = new Warrior();
-            else if (pick == 2) position = new Wizard();
+            if (pick == 1) {
+                position = new Warrior();
+            } else if (pick == 2) position = new Wizard();
 
         } else if (type == Rule.ORC) {
-            System.out.println(">1.흑마법사   2.돌격병");
+            System.out.println(">[1]흑마법사   [2]돌격병");
             pick = scanner.nextInt();
-            if (pick == 1) position = new DarkMagician();
-            else if (pick == 2) position = new AssaultSoldier();
+            if (pick == 1) {
+                position = new DarkMagician();
+            } else if (pick == 2) position = new AssaultSoldier();
 
         } else if (type == Rule.ELF) {
-            System.out.println(">1.힐러   2.궁수");
+            System.out.println(">[1]힐러   [2]궁수");
             pick = scanner.nextInt();
-            if (pick == 1) position = new Healer();
-            else if (pick == 2) position = new Archer();
+            if (pick == 1) {
+                position = new Healer();
+            } else if (pick == 2) position = new Archer();
         }
-        System.out.println(">" + position.getPositionType() + "(으)로 전직하였습니다.");
+        System.out.println("----------------------------------------------------");
+        System.out.println("*** " + position.getPositionType() + "(으)로 전직하였습니다 ***");
+        System.out.println("----------------------------------------------------");
         user.setPosition(position);
+        return user;
+    }
+
+    //무기장착
+    public User addWeapon(User user) {
+        String position = user.getPositionToString();
+        int pick;
+        Weapon weapon = null;
+
+        System.out.println(">무기를 선택하세요.");
+        if (position.equals(Rule.WARRIOR) || position.equals(Rule.ASSAULT_SOLDIER)) {
+            System.out.println(">[1]검   [2]도끼");
+            pick = scanner.nextInt();
+            if (pick == 1) weapon = new Sword();
+            else if (pick == 2) weapon = new Ax();
+
+        } else if (position.equals(Rule.DARK_MAGICIAN) || position.equals(Rule.HEALER) || position.equals(Rule.WIZARD)) {
+            System.out.println(">[1]지팡이");
+            pick = scanner.nextInt();
+            if (pick == 1) weapon = new Stick();
+
+        } else if (position.equals(Rule.ELF)) {
+            System.out.println(">[1]활");
+            pick = scanner.nextInt();
+            if (pick == 1) weapon = new Bow();
+        }
+        user.setWeapon(weapon);
+        System.out.println("----------------------------------------------------");
+        System.out.println("*** " + user.getWeaponToString() + "을(를) 장착했습니다 ***");
+        System.out.println("----------------------------------------------------");
+
+        return user;
+    }
+
+    //몬스터사냥
+    public User HuntingMonsters(User user) {
+        Weapon weapon = user.getWeapon();
+        Position position = user.getPosition();
+        int pick = 0;
+
+        //무기 내구도 체크
+        if (weapon.durabilityCheck()) {
+            System.out.println(">사용할 스킬을 선택하세요");
+            System.out.println(">[1]기본스킬:힐   [2]기본스킬:분노   [3]특수스킬");
+
+            pick = scanner.nextInt();
+
+            if (pick == 1) user.skillToHeal();
+            else if (pick == 2) user.skillToAnger();
+            else if (pick == 3) position.specialAttack(weapon);
+            weapon.subDurability();
+        }
         return user;
     }
 
