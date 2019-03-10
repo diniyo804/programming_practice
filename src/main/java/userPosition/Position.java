4795baaaede21235978f3bae0b4940f4ffaa9edd
diniyo804 <diniyo@naver.com>
2019-03-11 05:45:58 +0900

    position class 특수스킬 정보 추가

--- src/main/java/userPosition/Position.java
+++ src/main/java/userPosition/Position.java
@@ -3,11 +3,11 @@ package userPosition;
 import weapons.Weapon;
 
 public abstract class Position {
-    //2차직업 추상클래스
-
-
     //2차직업타입
-    String positionType;
+    private String positionType;
+
+    //특수스킬정보
+    private String specialSkill;
 
 
     // 1. 기본공격한다 (내구도 체크 후 공격)
@@ -15,18 +15,18 @@ public abstract class Position {
         if (weapon.durabilityCheck()) {
             System.out.println(">공격합니다...");
             weapon.subDurability();
+            System.out.println(">현재내구도 : " +weapon.getDurability());
         }
-        System.out.println(">무기의 수리가 필요합니다...");
+        else System.out.println(">무기의 수리가 필요합니다...");
     }
 
     // 2. 특수스킬을 사용한다
     public void specialAttack(Weapon weapon) {
-        if(weapon.durabilityCheck()){
-            System.out.println(">특수스킬을 시전합니다...");
-            //특수스킬 사용
-        }
-        System.out.println(">무기의 수리가 필요합니다...");
-
+        if (weapon.durabilityCheck()) {
+            System.out.println("----------------------------------------------------");
+            System.out.println(">특수스킬을 시전합니다");
+            System.out.println(specialSkill);
+        } else System.out.println(">무기의 수리가 필요합니다...");
     }
 
 
@@ -42,4 +42,12 @@ public abstract class Position {
         return this.positionType;
     }
 
+    //특수스킬정보
+    public String getSpecialSkill() {
+        return specialSkill;
+    }
+
+    public void setSpecialSkill(String specialSkill) {
+        this.specialSkill = specialSkill;
+    }
 }
