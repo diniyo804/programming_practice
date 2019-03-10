f6b5baa48d64975370e34c4a39ba650aa6f2f965
diniyo804 <diniyo@naver.com>
2019-03-11 05:45:04 +0900

    Rule class 변수 추가 및 값 변경

--- src/main/java/common/Rule.java
+++ src/main/java/common/Rule.java
@@ -6,23 +6,29 @@ public class Rule {
     public final static int MAX_LEVEL = 99;
     public final static int BASE_LEVEL = 1;
 
-    //종족(type)
-    public static final String HUMAN = "HUMAN";
-    public static final String ORC = "ORC";
-    public static final String ELF = "ELF";
+    //종족 종류 (type)
+    public static final String HUMAN = "인간";
+    public static final String ORC = "오크";
+    public static final String ELF = "엘프";
 
     //종족 선택시 번호
     public static final int PICK_HUMAN = 1;
     public static final int PICK_ORC = 2;
     public static final int PICK_ELF = 3;
 
-    //2차직업(position)
-    public static final String WARRIOR ="WARRIOR";
-    public static final String WIZARD ="WIZARD";
-    public static final String ASSAULT_SOLDIER ="ASSAULT_SOLDIER";
-    public static final String DARK_MAGICIAN ="DARK_MAGICIAN";
-    public static final String HEALER ="HEALER";
-    public static final String ARCHER = "ARCHER";
+    //2차직업 종류 (position)
+    public static final String WARRIOR ="전사";
+    public static final String WIZARD ="마법사";
+    public static final String ASSAULT_SOLDIER ="돌격병";
+    public static final String DARK_MAGICIAN ="흑마법사";
+    public static final String HEALER ="힐러";
+    public static final String ARCHER = "궁수";
+
+    //무기 종류 (weapon)
+    public static final String AX = "도끼";
+    public static final String BOW = "활";
+    public static final String STICK = "지팡이";
+    public static final String SWORD = "검";
 
     //무기 내구도
     public static final int BASIC_DURABLIITY = 100;
